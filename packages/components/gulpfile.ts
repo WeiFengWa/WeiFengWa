4d2764b9d92872be4e89eba51d665a76b53b615e
weifengwa <2969079275@qq.com>
2024-10-24 23:16:09 +0800

    优化打包

--- packages/components/gulpfile.ts
+++ packages/components/gulpfile.ts
@@ -9,7 +9,12 @@ import postcss from 'gulp-postcss'
 import concat from 'gulp-concat'
 import autoprefixer from 'autoprefixer'
 import { run, withTaskName } from '../../build/utils'
-import { styleRoot, outDir } from '../../build/utils/paths'
+import {
+  styleRoot,
+  componentRoot,
+  outDir,
+  weifengRoot
+} from '../../build/utils/paths'
 
 function r(p: string) {
   return path.resolve(__dirname, p)
@@ -44,9 +49,18 @@ const copyFullStyle = (p: string) => {
   return src(r(p)).pipe(dest(r(`${outDir}/styles`)))
 }
 
+// 把编译好的 css 拷贝到根目录下 dist 中
+const copyPackage = (p: string) => {
+  return src(r(p)).pipe(dest(r(outDir)))
+}
+
 export default series(
-  // withTaskName('cleanCss', () => run('rm -rf ./styles', styleRoot)),
-  withTaskName('buildCss', () => tailwindToCss('./src/*.css')),
-  withTaskName('mergeCss', () => mergeCss(`${outDir}/styles/*.css`))
-  // withTaskName('copyCss', () => copyFullStyle('./styles/**'))
+  withTaskName('clearOut', () => run('rm -rf ../dist', outDir)),
+  withTaskName('buildComponents', () => run('vite build', componentRoot)),
+  withTaskName('buildCommonCss', () => tailwindToCss(`${styleRoot}/src/common.css`)),
+  // withTaskName('mergeCss', () => mergeCss(`${outDir}/es/styles/src/*.css`)),
+  withTaskName('buildStyles', () => run('npm run build', styleRoot)),
+  // withTaskName('rmEsStyles', () => run('cd es && rm -rf styles', outDir)),
+  // withTaskName('rmLibStyles', () => run('cd lib && rm -rf styles', outDir)),
+  withTaskName('copyPackage', () => copyPackage(`${weifengRoot}/package.json`))
 )
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import { resolve } from 'path'
+import postcss from 'postcss'
+import { styleRoot, componentRoot, outDir } from '../../build/utils/paths'
 
 export default defineConfig({
   build: {
@@ -18,9 +20,9 @@ export default defineConfig({
       formats: ['es', 'umd', 'cjs']
     },
     rollupOptions: {
-      //忽略打包vue文件
-      external: ['vue'],
-      // input: ['index.ts'],
+      //忽略打包vue文件, /\.css/
+      external: ['vue', '@iconify/vue', '@vueuse/core', /\.css/],
+      input: ['index.ts'],
       output: [
         {
           //打包格式
@@ -31,21 +33,23 @@ export default defineConfig({
           preserveModules: true,
           exports: 'named',
           //配置打包根目录
-          dir: '../dist/es',
+          dir: `${outDir}/es`,
           globals: {
-            vue: 'Vue'
+            vue: 'Vue',
+            '@iconify/vue': 'IconifyVue',
+            '@vueuse/core': 'VueUse'
           }
         },
         {
           //打包格式
           format: 'cjs',
           //打包后文件名
-          entryFileNames: '[name].js',
+          entryFileNames: '[name].cjs',
           //让打包目录和我们目录对应
           preserveModules: true,
           exports: 'named',
           //配置打包根目录
-          dir: '../dist/lib'
+          dir: `${outDir}/lib`
         }
       ]
     }
@@ -53,14 +57,47 @@ export default defineConfig({
   plugins: [
     vue(),
     dts({
-      include: ['./**/*'],
-      entryRoot: 'src',
+      include: ['../components/**/*'],
+      exclude: ['../components/**/vite.config.ts'],
+      entryRoot: '../components',
+      cleanVueFileName: true,
       // packages/components
-      outDir: ['../dist/es/src', '../dist/lib/src']
+      outDir: [`${outDir}/es/components`, `${outDir}/lib/components`]
       // outputDir: ['../dist/es/src', '../easyest/lib/src'],
       //指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
       // tsConfigFilePath: '../../tsconfig.json'
     }),
-    DefineOptions()
-  ]
+    DefineOptions(),
+    {
+      name: 'style',
+      generateBundle(config, bundle) {
+        //这里可以获取打包后的文件目录以及代码code
+        const keys = Object.keys(bundle)
+
+        for (const key of keys) {
+          const bundler: any = bundle[key as any]
+          //rollup内置方法,将所有输出文件code中的.less换成.css,因为我们当时没有打包less文件
+          // console.log(bundler)
+          if(key.indexOf('button') > -1) {
+            console.log(bundler)
+          }
+          this.emitFile({
+            type: 'asset',
+            fileName: key, //文件名名不变
+            source: bundler.code
+              ?.replace(/@weifengwa\/components/g, 'weifengwa/es/components')
+              ?.replace(/import\s+['"]@weifengwa\/styles\/src\/[^'"]+\.css['"];/g, '')
+              ?.replace(/require\("@weifengwa\/styles\/src\/([a-zA-Z0-9_-]+)\.css"\);/g, '')
+              ?.replace(/@weifengwa\/styles\/src/g, 'weifengwa/styles')
+          })
+        }
+      }
+    }
+  ],
+  css: {
+    postcss: './postcss.config.js' // 确保使用正确的 PostCSS 配置
+  }
+  // resolve: {
+  //   alias: [{ find: '@weifengwa/styles/src', replacement: 'weifengwa/styles' }]
+  // }
 })
